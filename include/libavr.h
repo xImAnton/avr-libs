70ce1003477e82573ba79c393891e4c208a02a85
Anton Vogelsang <anton.vogelsang@outlook.de>
2021-12-23 14:40:54 +0100

    added ADC support into libavr

--- include/libavr.h
+++ include/libavr.h
@@ -20,7 +20,19 @@
 #define I_INT1_CHANGE (1 << ISC01)
 #define I_INT1_LOW 0x00
 
-#define delay(ms) { uint32_t t = ms; while (t--) { _delay_ms(1); } }
+#define delay(ms) uint32_t _t = ms; while (_t--) _delay_ms(1)
+
+#define adc_enable() ADCSRA |= 1 << ADEN
+#define adc_disable() ADCSRA &= ~(1 << ADEN)
+#define adc_prescale(x) ADCSRA &= 0xF8 | ((x) & 7)
+
+#define ADC_PRESC_2   1
+#define ADC_PRESC_4   2
+#define ADC_PRESC_8   3
+#define ADC_PRESC_16  4
+#define ADC_PRESC_32  5
+#define ADC_PRESC_64  6
+#define ADC_PRESC_128 7
 
 void pin_set_mode(volatile uint8_t *ddr, uint8_t pin, uint8_t mode) {
     if (mode) {
@@ -44,6 +56,16 @@ void pin_set(volatile uint8_t *port, uint8_t pin, uint8_t signal) {
 inline void i_setup(uint8_t interrupt, uint8_t mode) {
     GICR |= ( 1 << interrupt );
     MCUCR |= mode;
+}
+
+uint16_t adc_read(uint8_t channel) {
+    ADMUX |= channel & 0x0f;
+    ADCSRA |= (1 << ADSC);
+
+    while((ADCSRA&(1<<ADIF))==0);
+
+    _delay_us(10);
+    return ADCL | (ADCH << 8);
 }
 
 #endif // not __libavr_h_included__
